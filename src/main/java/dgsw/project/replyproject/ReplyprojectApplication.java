b9029563788d0c978d9b2b481ae40249bbc6bb9d
BuildTools <unconfigured@null.spigotmc.org>
2022-12-06 14:47:10 +0900

    Initial commit

---
+++ src/main/java/dgsw/project/replyproject/ReplyprojectApplication.java
@@ -0,0 +1,13 @@
+package dgsw.project.replyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplyprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dgsw/project/replyproject/ReplyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package dgsw.project.replyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
